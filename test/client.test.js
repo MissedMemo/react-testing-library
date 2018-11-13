1dc9636e1d086bedcc60f139e6cefdc59b2eec80
AndyL <missedmemo@gmail.com>
2018-11-13 15:07:28 -0800

    define simplest testing lib routines (test & expect)

--- test/client.test.js
+++ test/client.test.js
@@ -1,16 +1,30 @@
+const expect = actual => ({
+  toBe: expected => {
+    if ( actual !== expected ) {
+      throw new Error(`${result} does not equal value: ${expected}`)
+    }
+  }
+})
+
+const test = ( title, callback ) => {
+  try {
+    console.log(`✔︎ ${title}`)
+  } catch(err) {
+    console.log(`✘ ${title}`)
+  }
+}
+
 const sum = (a,b) => a + b;
 const subtract = (a,b) => a - b;
 
-let result = sum(3,7);
-let expected = 10;
+test( 'should sum values', () => {
+  const result = sum(7,3);
+  const expected = 10;
+  expect(result).toBe(expected)
+})
 
-if ( result !== expected ) {
-  throw new Error( `${result} does not equal value: ${expected}`)
-}
-
-result = subtract(7,3);
-expected = 4;
-
-if ( result !== expected ) {
-  throw new Error( `${result} does not equal value: ${expected}`)
-}
+test( 'should subtract values', () => {
+  const result = subtract(7,3);
+  const expected = 4;
+  expect(result).toBe(expected)
+})
