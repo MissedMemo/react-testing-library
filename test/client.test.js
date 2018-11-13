fb0d50be0b821c3e7d281680a53e3115b9884910
AndyL <missedmemo@gmail.com>
2018-11-13 15:40:57 -0800

    fix testing lib bug

--- test/client.test.js
+++ test/client.test.js
@@ -8,13 +8,15 @@ const expect = actual => ({
 
 const test = ( title, callback ) => {
   try {
+    callback();
     console.log(`✔︎ ${title}`)
   } catch(err) {
     console.log(`✘ ${title}`)
+    console.log(err)
   }
 }
 
-const sum = (a,b) => a + b;
+const sum = (a,b) => a - b;
 const subtract = (a,b) => a - b;
 
 test( 'should sum values', () => {
