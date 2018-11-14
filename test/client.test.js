2967520293edafa74680e0e7ebcd3cbfd577b2d6
AndyL <missedmemo@gmail.com>
2018-11-13 22:00:16 -0800

    declare lib methods 'global' to eliminate need for clients to import

--- test/client.test.js
+++ test/client.test.js
@@ -1,27 +1,26 @@
-const _ = require('../lib/testinglib');
 const { sum, sumAsync, subtract, subtractAsync } = require('../client.js');
 
 
-_.test( 'should sum values', () => {
+test( 'should sum values', () => {
   const result = sum(7,3);
   const expected = 10;
-  _.expect(result).toBe(expected)
+  expect(result).toBe(expected)
 })
 
-_.test( 'should subtract values', () => {
+test( 'should subtract values', () => {
   const result = subtract(7,3);
   const expected = 4;
-  _.expect(result).toBe(expected)
+  expect(result).toBe(expected)
 })
 
-_.test( 'should sum values asyncronously', async () => {
+test( 'should sum values asyncronously', async () => {
   const result = await sumAsync(7,4)
   const expected = 11;
-  _.expect(result).toBe(expected)
+  expect(result).toBe(expected)
 })
 
-_.test( 'should subtract values asyncronously', async () => {
+test( 'should subtract values asyncronously', async () => {
   const result = await subtractAsync(7,2);
   const expected = 4;
-  _.expect(result).toBe(expected)
+  expect(result).toBe(expected)
 })
