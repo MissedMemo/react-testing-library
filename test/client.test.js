453b4f179fb014149db6eef3295e126e25834919
AndyL <missedmemo@gmail.com>
2018-11-13 16:03:36 -0800

    use abbreviated lib routine reference syntax

--- test/client.test.js
+++ test/client.test.js
@@ -1,15 +1,15 @@
-const { test, expect } = require('../lib/testinglib');
+const _ = require('../lib/testinglib');
 const { sum, subtract } = require('../client.js');
 
 
-test( 'should sum values', () => {
+_.test( 'should sum values', () => {
   const result = sum(7,3);
   const expected = 10;
-  expect(result).toBe(expected)
+  _.expect(result).toBe(expected)
 })
 
-test( 'should subtract values', () => {
+_.test( 'should subtract values', () => {
   const result = subtract(7,3);
   const expected = 4;
-  expect(result).toBe(expected)
+  _.expect(result).toBe(expected)
 })
