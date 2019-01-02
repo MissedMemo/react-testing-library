e2ad48ea2e7c5aa6af7df48811aeec0a8a6694ca
AndyL <missedmemo@gmail.com>
2019-01-01 16:13:34 -0800

    mimic explicit tuts structure

---
+++ 1-fundamentals/1-simple/client.js
@@ -0,0 +1,10 @@
+
+/*
+const sum = (a,b) => a + b;
+const subtract = (a,b) => a - b;
+
+sumAsync = (...args) => Promise.resolve( sum(...args) )
+subtractAsync = (...args) => Promise.resolve( subtract(...args) )
+
+module.exports = { sum, sumAsync, subtract, subtractAsync }
+*/
