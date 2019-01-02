5fd51ba4a670eaa87dce398b2411ce382b48b4e3
AndyL <missedmemo@gmail.com>
2019-01-01 21:19:33 -0800

    demo complete

--- 1-fundamentals/1-simple/client.js
+++ 1-fundamentals/1-simple/client.js
@@ -1,8 +1,21 @@
+const sum = (a,b) => a + b
+const subtract = (a,b) => a - b
+
+let result = sum(3,4)
+let expected = 7
+
+if ( result !== expected ) {
+  throw Error(`${result} did not equal ${expected}`)
+}
+
+result = subtract(4,3)
+expected = 1
+
+if ( result !== expected ) {
+  throw Error(`${result} did not equal ${expected}`)
+}
 
 /*
-const sum = (a,b) => a + b;
-const subtract = (a,b) => a - b;
-
 sumAsync = (...args) => Promise.resolve( sum(...args) )
 subtractAsync = (...args) => Promise.resolve( subtract(...args) )
 
