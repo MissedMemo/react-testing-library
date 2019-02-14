2f7e467606ad10aeb7c03579d221c75b6f266209
AndyL <missedmemo@gmail.com>
2019-02-13 22:25:26 -0800

    create Hook-based version of component

--- 5-react/1-testlibs-evolution/src/favnum-hook-based.jsx
+++ 5-react/1-testlibs-evolution/src/favnum-hook-based.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import styled from '@emotion/styled'
+import React, { useState } from 'react'
 
-const FavoriteNumber = () => {
-  return <div>3</div>
+const SpacedLabel = styled.label`
+  font-size: 24px;
+  color: blue;
+  :after {
+    content: ':  ';
+  }
+`
+
+const FavoriteNumber = ({min = 1, max = 9}) => {
+
+  const [number, setNumber] = useState(0)
+  const [numberEntered, setNumberEntered] = useState(false)
+  
+  const handleChange = ({target}) => {
+    setNumber(target.value)
+    setNumberEntered(true)
+  }
+
+  const isValid = !numberEntered || ( number >= min && number <= max )
+  
+  return <div>
+    <SpacedLabel htmlFor='numInput'>Favorite Number</SpacedLabel>
+    <input type='number' id='numInput' value={number} onChange={handleChange} />
+    {
+      isValid ? null : <div>Your number is invalid</div>
+    }
+  </div>
 }
 
 export default FavoriteNumber
