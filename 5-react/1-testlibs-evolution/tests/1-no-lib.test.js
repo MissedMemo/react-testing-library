ab485def89d87deb400ad98191c5206a53871cd3
AndyL <missedmemo@gmail.com>
2019-02-13 22:32:53 -0800

    test hook and class-based component versions side-by-side

--- 5-react/1-testlibs-evolution/tests/1-no-lib.test.js
+++ 5-react/1-testlibs-evolution/tests/1-no-lib.test.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-import FavoriteNumber from '../src/favnum-class-based'
+import FavNumClass from '../src/favnum-class-based'
+import FavNumHook from '../src/favnum-hook-based'
 
-test('render input field and label', () => {
-  const node = document.createElement('div')
-  render( <FavoriteNumber />, node )
-  expect(node.querySelector('input').type).toBe('number')
-  expect( node.querySelector('label').textContent ).toBe('Favorite Number')
+describe('traditional class-based component', () => {
+
+  test('render input field and label', () => {
+    const node = document.createElement('div')
+    render( <FavNumClass />, node )
+    expect(node.querySelector('input').type).toBe('number')
+    expect( node.querySelector('label').textContent ).toBe('Favorite Number')
+  })
+})
+
+describe('new hook-based component', () => {
+
+  test('render input field and label', () => {
+    const node = document.createElement('div')
+    render( <FavNumHook />, node )
+    expect(node.querySelector('input').type).toBe('number')
+    expect( node.querySelector('label').textContent ).toBe('Favorite Number')
+  })
 })
