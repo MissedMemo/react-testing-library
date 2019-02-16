ab9e18c50ff2084f33ae7a892cd79dcce5c7f02e
AndyL <missedmemo@gmail.com>
2019-02-16 14:22:48 -0800

    implement accessibility tests using jest-axe

--- 5-react/2-forms/tests/1-a11y-jest-axe.test.js
+++ 5-react/2-forms/tests/1-a11y-jest-axe.test.js
@@ -1,17 +1,18 @@
 import 'jest-dom/extend-expect'
 import 'react-testing-library/cleanup-after-each'
+import 'jest-axe/extend-expect'
 import React from 'react'
 import { render } from 'react-testing-library'
 import { axe } from 'jest-axe'
 
 
 const Form = () => <form>
-  <input placeholder='user name' name='username' />
+  <label htmlFor='username' >User Name</label>
+  <input id='username' placeholder='User Name' />
 </form>
 
 test('form is accessible', async () => {
   const { container } = render( <Form /> )
   const results = await axe( container.innerHTML )
-  console.log(results)
-  expect( 1 +1 ).toBe(2)
+  expect( results ).toHaveNoViolations()
 })
