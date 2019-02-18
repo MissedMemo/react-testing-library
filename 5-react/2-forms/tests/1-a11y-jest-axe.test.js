90d7831003eebfe1d72261daf20dab5b7dc446b9
AndyL <missedmemo@gmail.com>
2019-02-17 19:08:59 -0800

    jest-axe failing to parse properly

--- 5-react/2-forms/tests/1-a11y-jest-axe.test.js
+++ 5-react/2-forms/tests/1-a11y-jest-axe.test.js
@@ -1,4 +1,3 @@
-import 'jest-dom/extend-expect'
 import 'react-testing-library/cleanup-after-each'
 import 'jest-axe/extend-expect'
 import React from 'react'
@@ -6,13 +5,23 @@ import { render } from 'react-testing-library'
 import { axe } from 'jest-axe'
 
 
-const Form = () => <form>
-  <label htmlFor='username' >User Name</label>
-  <input id='username' placeholder='User Name' />
+const InaccessibleForm = () => <form>
+  <input name='username' placeholder='User Name' />
 </form>
 
+const AccessibleForm = () => <form>
+  <label htmlFor='uniqueid' >User Name</label>
+  <input id='uniqueid' placeholder='User Name' />
+</form>
+
+test('inaccessible form FAILS axe test', async () => {
+  const { container } = render( <InaccessibleForm /> )
+  const results = await axe( container.innerHTML )
+  expect( results ).not.toHaveNoViolations()
+})
+
 test('form is accessible', async () => {
-  const { container } = render( <Form /> )
+  const { container } = render( <AccessibleForm /> )
   const results = await axe( container.innerHTML )
   expect( results ).toHaveNoViolations()
 })
