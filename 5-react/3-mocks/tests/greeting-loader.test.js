7321a1ae520a7f50c9fb3d18f6d815ea6f358ac4
AndyL <missedmemo@gmail.com>
2019-02-20 16:04:51 -0800

    mock async api method for testing

--- 5-react/3-mocks/tests/greeting-loader.test.js
+++ 5-react/3-mocks/tests/greeting-loader.test.js
@@ -2,6 +2,12 @@ import React from 'react'
 import { render, fireEvent, wait } from 'react-testing-library'
 import GreetingLoader from '../src/greeting-loader'
 
+jest.mock( '../src/greeting-api', () => {
+  return {
+    loadGreeting: jest.fn( subject => Promise.resolve(`Hello, ${subject}!`) )
+  }
+})
+
 test( 'should load greeting on click', async () => {
   const { getByLabelText, getByText, getByTestId } = render( <GreetingLoader /> )
   const input = getByLabelText( /name:/i )
