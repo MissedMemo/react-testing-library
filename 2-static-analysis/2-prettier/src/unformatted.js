4385b3b73e6bb73ba8467201ea84a5376275908c
AndyL <missedmemo@gmail.com>
2019-09-11 22:20:12 -0700

    add basic formatting support

---
+++ 2-static-analysis/2-prettier/src/unformatted.js
@@ -0,0 +1,12 @@
+class Dog {
+  constructor(breed = "setter") {
+    this.breed = breed;
+  }
+
+  bark() {
+    console.log(`${this.breed} says 'woof!'`);
+  }
+}
+
+const fifi = new Dog("poodle");
+fifi.bark();
