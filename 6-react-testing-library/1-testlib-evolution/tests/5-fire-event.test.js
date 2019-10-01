44b6c8437c376b328a13a1f8428fec2de57830e9
AndyL <missedmemo@gmail.com>
2019-09-30 22:06:41 -0700

    rerender component with updated props

--- 6-react-testing-library/1-testlib-evolution/tests/5-fire-event.test.js
+++ 6-react-testing-library/1-testlib-evolution/tests/5-fire-event.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render, fireEvent } from '@testing-library/react'
+import { render, fireEvent, queryByTestId } from '@testing-library/react'
 import FavNumClass from '../src/favnum-class-based'
 import FavNumFunctional from '../src/favnum-functional'
 
@@ -30,10 +30,12 @@ describe('functional/hook-based component', () => {
   })
 
   test('entering invalid value displays error message', () => {
-    const { getByLabelText, getByTestId, debug } = render( <FavNumFunctional /> )
+    const { getByLabelText, getByTestId, queryByTestId, debug, rerender } = render( <FavNumFunctional /> )
     const input = getByLabelText( /favorite number/i )
     fireEvent.change( input, { target: { value: 10}})
-    debug(input)
     expect( getByTestId('error-message')).toHaveTextContent( /the number is invalid/i )
+    rerender(<FavNumFunctional max={12} />)
+    debug(input) // value should still be 10, but NO ERROR (since passing new props)
+    expect( queryByTestId('error-message') ).toBeNull()
   })
 })
