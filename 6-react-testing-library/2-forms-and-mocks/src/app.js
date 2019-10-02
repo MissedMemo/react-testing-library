7094b702de0ef720e24235e662f1fd9ea3c1c06e
AndyL <missedmemo@gmail.com>
2019-10-02 13:51:16 -0700

    remove unused placeholder files

--- 6-react-testing-library/2-forms-and-mocks/src/app.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const App = ({greeting}) => <>
-  { greeting }
-</>
-
-export default App
--- 6-react-testing-library/2-forms-and-mocks/tests/app.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-import App from '../src/app'
-
-test('validate test setup...', () => {
-  const { getByText } = render(<App greeting="Hello, React!" />)
-  getByText( /hello, react!/i ) // throws error if not found in component
-})
