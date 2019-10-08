ecc419d614010fc1bcd4c1f81638a63d093f25a6
AndyL <missedmemo@gmail.com>
2019-10-07 19:26:53 -0700

    test added, but failing to run...

--- 6-react-testing-library/2-forms-and-mocks/tests/4-error-boundary.test.js
+++ 6-react-testing-library/2-forms-and-mocks/tests/4-error-boundary.test.js
@@ -2,11 +2,40 @@ import '@testing-library/jest-dom/extend-expect' // belongs in jest setup...
 
 import React from 'react'
 import { render, fireEvent } from '@testing-library/react'
-import expectExport from 'expect'
+import ErrorBoundary from '../src/components/error-boundary'
+import { reportError as mockReportError } from '../src/api'
 
-describe('ErrorBoundary....', () => {
-  
-  test('place-holder...', () => {
-    expectExport(1+1).toBe(2)
-  })
+beforeEach( () => {
+  // temporarily override console error output to prevent extraneous warnings etc. on throw....
+  jest.spyOn( console, 'error' ).mockImplementation( () => {} )
+})
+
+afterEach( () => {
+  console.error.mockRestore()
+})
+
+// mock the api call, without any delay...
+jest.mock( '../src/api', () => {
+  reportError: jest.fn( () => Promise.resolve({ success: true }) )
+})
+
+const BadComponent = ({ shouldThrow }) => {
+  if ( shouldThrow ) {
+    throw Error('💣 BOOM!')
+  } else {
+    return null
+  }
+}
+
+test('calls report error and registers a problem...', () => {
+
+  const { container, rerender, getByText } = render( <ErrorBoundary><BadComponent /></ErrorBoundary> )
+
+  /*
+  rerender( <ErrorBoundary>
+    <BadComponent shouldThrow={true} />
+  </ErrorBoundary> )
+    */
+
+  expect( mockReportError ).toHaveBeenCalledTimes(1)
 })
