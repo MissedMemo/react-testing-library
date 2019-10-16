693e0caa483e4dd2dea820183a93e4aa922bdda0
AndyL <missedmemo@gmail.com>
2019-10-15 21:28:46 -0700

    fix bug in mock of error reporting method

--- 6-react-testing-library/2-forms-and-mocks/tests/4-error-boundary.test.js
+++ 6-react-testing-library/2-forms-and-mocks/tests/4-error-boundary.test.js
@@ -2,6 +2,7 @@ import '@testing-library/jest-dom/extend-expect' // belongs in jest setup...
 
 import React from 'react'
 import { render, fireEvent } from '@testing-library/react'
+import { reportError as mockReportError } from '../src/api'
 import ErrorBoundary from '../src/components/error-boundary'
 
 beforeEach( () => {
@@ -14,15 +15,15 @@ afterEach( () => {
 })
 
 // mock the api call, without any delay...
-jest.mock( '../src/api', () => {
+jest.mock( '../src/api.js', () => ({
   reportError: jest.fn( () => Promise.resolve({ success: true }) )
-})
+}))
 
 const BadComponent = ({ shouldThrow }) => {
   if ( shouldThrow ) {
     throw Error('💣 BOOM!')
   } else {
-    return null
+    return <>All Good!</>
   }
 }
 
@@ -30,10 +31,13 @@ test('calls report error and registers a problem...', () => {
 
   const { container, rerender, getByText } = render( <ErrorBoundary><BadComponent /></ErrorBoundary> )
 
+  expect(container).toHaveTextContent('All Good!')
+
   rerender( <ErrorBoundary>
     <BadComponent shouldThrow={true} />
   </ErrorBoundary> )
 
-  //expect( mockReportError ).toHaveBeenCalledTimes(1)
   expect(container).toHaveTextContent('Whoops!')
+  expect(console.error).toHaveBeenCalledTimes(2) // once by JSDom & once by React
+  expect( mockReportError ).toHaveBeenCalledTimes(1)
 })
