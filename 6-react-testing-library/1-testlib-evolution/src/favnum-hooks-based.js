932b19b8026041bafb05427da09c4ca5e3bd30bc
AndyL <missedmemo@gmail.com>
2019-11-01 17:46:40 -0700

    add tests for events, and identify div etc. tags via testid

--- 6-react-testing-library/1-testlib-evolution/src/favnum-hooks-based.js
+++ 6-react-testing-library/1-testlib-evolution/src/favnum-hooks-based.js
@@ -13,7 +13,7 @@ const FavNum = () => {
   return <>
     <label htmlFor='favnum'>Favorite Number</label>
     <input id='favnum' type='number' onChange={handleChange} value={number} />
-    { isValid || <div>
+    { isValid || <div data-testid='error-message'>
       Number is INVALID!
     </div>}
   </>
