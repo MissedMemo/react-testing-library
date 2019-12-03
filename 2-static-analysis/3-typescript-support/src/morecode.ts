36d37c6f6ca3c2529d6df2437102b3c84a588e30
AndyL <missedmemo@gmail.com>
2019-12-03 09:04:10 -0800

    add basic TypeScript support to babel compilation

---
+++ 2-static-analysis/3-typescript-support/src/morecode.ts
@@ -0,0 +1,44 @@
+/*
+function add(a: number, b: number): number {
+  return a + b
+}
+*/
+
+const add = (a: number, b: number): number => a + b
+
+interface User {
+  name: {
+    first: string
+    middle: string
+    last: string
+  }
+}
+
+function getFullName(user: User): string {
+  const {
+    name: {first, middle, last},
+  } = user
+  return [first, middle, last].filter(Boolean).join('')
+}
+
+add(1, 2)
+
+getFullname({name: {first: 'Joe', middle: 'Bud', last: 'Matthews'}})
+
+/* Original version... (without TypeScript)
+
+function add(a, b) {
+  return a + b
+}
+
+function getFullName(user) {
+  const {
+    name: {first, middle, last},
+  } = user
+  return [first, middle, last].filter(Boolean).join('')
+}
+
+add(1, 'two')
+
+getFullName({name: {first: 'Joe', midd1e: 'Bud', last: 'Matthews'}})
+*/
