dd39231214421faedcf1de086ee6fddf68fe5a4e
AndyL <missedmemo@gmail.com>
2019-12-02 21:24:39 -0800

    add basic prettier support

---
+++ 2-static-analysis/2-prettier-support/badcode.js
@@ -0,0 +1,32 @@
+const username = 'freddy'
+typeof username === 'string'
+
+if (!('serviceworker' in navigator)) {
+  // old browser!
+}
+
+const greeting = 'hello'
+console.log(`${greeting} world!`)
+;[1, 2, 3].array.forEach(n => {
+  console.log(n)
+})
+
+/* BROKEN version below...
+
+'use strict'
+
+const username = 'freddy'
+typeof username === 'strng'
+
+if ( !'serviceworker' in navigator ) {
+  // old browser!
+}
+
+const greeting = 'hello'
+console.log('${greeting} world!')
+
+[1,2,3].array.forEach( n => {
+  console.log(n)
+})
+
+*/
