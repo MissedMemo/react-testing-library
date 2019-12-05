8c18a9ed498dd13d0728088b007f2bcaaf78a03d
AndyL <missedmemo@gmail.com>
2019-12-04 22:33:34 -0800

    support babel 'modules' switch between test vs. standard modes

--- 4-jest-config-options/.babelrc.js
+++ 4-jest-config-options/.babelrc.js
@@ -1,3 +1,7 @@
+/* jest runs in node with modules set to commonjs, switching to false otherwise, to support tree-shaking */
+
+const isTest = String(process.env.NODE_ENV) === 'test'
+
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react']
+  presets: [ ['@babel/preset-env', { modules: isTest ? 'commonjs' : false } ], '@babel/preset-react' ]
 }
--- 4-jest-config-options/tests/example.test.js
+++
@@ -1,3 +0,0 @@
-test( 'make sure it installed ok', () => {
-  expect(1+1).toBe(2)
-})
