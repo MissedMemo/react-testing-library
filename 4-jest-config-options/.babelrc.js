a22b679c259a79d47169893d0dd16c4778fe077e
AndyL <missedmemo@gmail.com>
2019-12-04 22:52:14 -0800

    add basic lint/prettier configs

--- 4-jest-config-options/.babelrc.js
+++ 4-jest-config-options/.babelrc.js
@@ -1,6 +1,6 @@
 /* jest runs in node with modules set to commonjs, switching to false otherwise, to support tree-shaking */
 
-const isTest = String(process.env.NODE_ENV) === 'test'
+const isTest = String(process.env.NODE_ENV) === 'test';;;
 
 module.exports = {
   presets: [ ['@babel/preset-env', { modules: isTest ? 'commonjs' : false } ], '@babel/preset-react' ]
