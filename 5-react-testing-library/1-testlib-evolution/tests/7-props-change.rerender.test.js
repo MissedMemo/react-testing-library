33e670378f8bdf6e739d1b3178f0fae1e3a9a45f
AndyL <missedmemo@gmail.com>
2020-01-14 15:21:54 -0800

    test props change via rerender

---
+++ 5-react-testing-library/1-testlib-evolution/tests/7-props-change.rerender.test.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import user from '@testing-library/user-event'
+import FavHooks from '../src/favnum-hooks-based'
+import FavClass from '../src/favnum-class-based'
+
+const errorMsgFragment = /invalid/i
+
+describe('test hooks-based component version', () => {
+
+  test('renders number input with appropriate label', () => {
+    const { getByLabelText } = render(<FavHooks />)
+
+    const input = getByLabelText( /favorite number/i )
+    expect(input).toHaveAttribute('type','number')
+  })
+
+  test('displays error on invalid input, with no error on valid input', () => {
+    const { getByLabelText, getByRole, queryByText, rerender } = render(<FavHooks />)
+    const input = getByLabelText( /favorite number/i )
+    
+    expect( queryByText( errorMsgFragment )).toBeFalsy()
+
+    fireEvent.change(input, {target: {value: '45'}})
+    expect( getByRole( /alert/i )).toHaveTextContent( errorMsgFragment )
+
+    rerender( <FavHooks max={45} />)
+    expect( queryByText( errorMsgFragment )).toBeFalsy()
+  })
+})
+
+
+describe('test class-based component version', () => {
+
+  test('renders number input with appropriate label', () => {
+    const { getByLabelText } = render(<FavClass />)
+
+    const input = getByLabelText( /favorite number/i )
+    expect(input).toHaveAttribute('type','number')
+  })
+
+  test('displays error on invalid input, with no error on valid input', () => {
+    const { getByLabelText, getByRole, queryByText, rerender } = render(<FavClass />)
+    const input = getByLabelText( /favorite number/i )
+    
+    expect( queryByText( errorMsgFragment )).toBeFalsy()
+
+    user.type( input, '45') //superior to fireEvent, to capture focus, keystrokes etc.
+    expect( getByRole( /alert/i )).toHaveTextContent( errorMsgFragment )
+
+    rerender( <FavClass max={45} />)
+    expect( queryByText( errorMsgFragment )).toBeFalsy()
+  })
+
+})
