ab046e18d0de68ed11df86460f28a273ed24d0f0
AndyL <missedmemo@gmail.com>
2020-01-13 19:37:19 -0800

    create basic component and vanilla test method

---
+++ 5-react-testing-library/1-testlib-evolution/src/favnum-hooks-based.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+
+const FavoriteNumber = ({min = 1, max = 42}) => {
+
+  const [num, setNum] = useState(0)
+  const [numberEntered, setNumberEntered] = useState(false)
+
+  const handleEdit = ({target}) => {
+    setNum( Number(target.value) )
+    setNumberEntered(true)
+  }
+
+  const isValid = !numberEntered || num >= min && num <= max
+
+  return <div>
+    <label htmlFor='favnum'>Favorite Number:</label>
+    <input id='favnum' type='number' onChange={handleEdit} value={num} />
+    { isValid || <div role='alert'>That number is Invalid!</div> }
+  </div>
+}
+
+export { FavoriteNumber }
--- 5-react-testing-library/1-testlib-evolution/tests/1-nolib.test.js
+++ 5-react-testing-library/1-testlib-evolution/tests/1-nolib.test.js
@@ -1,1 +1,10 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { FavoriteNumber } from '../src/favnum-hooks-based'
 
+test('renders number input with appropriate label', () => {
+  const div = document.createElement('div')
+  render( <FavoriteNumber />, div )
+  expect(div.querySelector('input').type).toBe('number')
+  expect(div.querySelector('label').textContent).toBe('Favorite Number:')
+})
